47492357f87f5ef49c707193c437c9d789cf49d8
angel <fsguoangel@hotmail.com>
2022-01-16 20:49:25 +0800

    init

---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostsController;
+use App\Http\Controllers\AboutController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/', [HomeController::class, 'home'])
+  ->name('home.index');
+Route::get('/contact', [HomeController::class, 'contact'])
+  ->name('home.contact');
+
+Route::get('/single', AboutController::class);
+$posts = [
+    1 => [
+      'title' => 'Intro to Laravel',
+      'content' => 'This is a short intro to Laravel',
+      'is_new' => true,
+      'has_comments' => true
+    ],
+    2 => [
+      'title' => 'Intro to PHP',
+      'content' => 'This is a short intro to PHP',
+      'is_new' => false
+    ],
+    3 => [
+      'title' => 'Intro to Golang',
+      'content' => 'This is a short intro to Golang',
+      'is_new' => false
+    ]
+  ];
+  Route::resource('posts', PostsController::class);
+// Route::get('/posts', function () use ($posts) {
+//   // dd(request()->all());
+//   dd((int)request()->query('page', 1));
+//   // compact($posts) === ['posts' => $posts])
+//   return view('posts.index', ['posts' => $posts]);
+// });
